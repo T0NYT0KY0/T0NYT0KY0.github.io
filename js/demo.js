54ddaffe4835cc1aaf1d9af218769292074cde73
Keith Adams <keith@iflow.dev>
2022-03-09 04:20:46 -0600

    Update index.html

---
+++ js/demo.js
@@ -0,0 +1,11 @@
+$(document).on("click", "#btn01", function () {
+    window.location.href = "https://www.youtube.com/watch?v=B2hY4g3fBxs";
+  });
+
+  $(document).on("click", "#btn02", function () {
+    window.location.href = "https://www.youtube.com/watch?v=75Tnm-7k8xM";
+  });
+
+  $(document).on("click", "#btn03", function () {
+    window.location.href = "https://www.youtube.com/watch?v=tPxr9q7vtas";
+  });
